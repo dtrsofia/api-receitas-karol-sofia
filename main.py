83abf19d593dd062aa026918aefd15014d29e5b4
Karol Baldan <karol.baldan12@aluno.ifce.edu.br>
2025-09-01 11:34:24 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -30,10 +30,49 @@ receitas = [
         'ingredientes': [
             'Batatas Inglesas',
             'Óleo',
-            'Sal',
+            '1 pitada de Sal',
         ],
         'utensílios': ['faca', 'panela'],
         'modo de preparo': 'Descasque e corte as batatas em formato de palitos. Esquente o óleo na panela e frite as batatas por imersão. Retire e adicione sal a gosto.'
+    },
+
+      {
+        'nome': 'Brigadeiro',
+        'ingredientes': [
+            '1 Caixa de Leite Condensado',
+            '1/2 Colher de Manteiga',
+            '3 Colheres de Achocolatado',
+        ],
+        'utensílios': ['colher', 'panela'],
+        'modo de preparo': 'Adicione os ingrendientes em uma panela e leve ao fogo até que se torne uma mistura homogênea. Para uma consistência perfeita espere o brigadeiro desgrudar do fundo da panela.'
+    },
+
+    {
+        'nome': 'Molho Branco',
+        'ingredientes': [
+            '2 xícaras de leite',
+            '2 colheres (sopa) de farinha de trigo',
+            'noz-moscada a gosto',
+            '2 colheres (sopa) de manteiga'
+            'sal a gosto'
+            'pimenta-do-reino branca a gosto'
+        ],
+        'utensílios': ['colher de pau', 'panela', 'faca', 'tábua de corte'],
+        'modo de preparo': 'Ferva o leite. Derreta a manteiga, junte a farinha e mexa bem até obter uma pasta homogênea. Aos poucos, acrescente o leite e bata, constantemente, para não empelotar. Deixe cozinhar por alguns minutos e tempere com sal, noz-moscada e pimenta.'
+    },
+
+    {
+        'nome': 'Salada de Frutas',
+        'ingredientes': [
+            '3 Bananas',
+            '3 Maças'
+            '3 Goiabas',
+            '1 Mamão'
+            '1 Caixa de Leite Condensado'
+            '1/2 Caixa de Creme de Leite'
+        ],
+        'utensílios': ['vasilha', 'faca', 'colher'],
+        'modo de preparo': 'Descasque e corte todas as frutas. Coloque dentro de um recipiciente juntamente com o leite condensado e o creme de leite.'
     },
 
 
